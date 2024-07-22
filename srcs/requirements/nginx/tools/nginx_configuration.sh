880d9fa52935a7b097b40b3b262b1c3273009906
Adnane Saber <70679159+don-frex@users.noreply.github.com>
2024-07-22 01:58:01 +0100

    Add files via upload

--- srcs/requirements/nginx/tools/nginx_configuration.sh
+++ srcs/requirements/nginx/tools/nginx_configuration.sh
@@ -3,4 +3,6 @@
 #create SSL Certificate
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MO/L=KH/O=1337/OU=student/CN=asaber.1337.ma"
 
+cat -e /etc/ssl/certs/nginx-selfsigned.crt
+ls -l /etc/nginx/sites-enabled
 nginx -g "daemon off;"
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,38 +1,13 @@
 #!/bin/bash
 
-
-sleep 5
-
-
-# sed -i "s/wordpress/$db1_name/g"   wp-config.php
-# sed -i "s/myuser/$db1_user/g"  wp-config.php
-# sed -i "s/mypassword/$db1_pwd/g"    wp-config.php
-# sed -i "s/db/$db1_name/g"    wp-config.php
-
-
-wp --allow-root --path=/var/www/html/ core download
-
-wp --allow-root --path=/var/www/html/ config create --dbname=$db1_name --dbuser=$db1_user --dbpass=$db1_pwd --dbhost=mariadb:3306
-
-wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL --skip-email --allow-root
-
-wp user create $db1_user $WP_UEMAIL --role=author --user_pass=$db1_pwd --allow-root
-
-wp theme install astra --activate --allow-root
-
-
-# wp plugin install redis-cache --activate --allow-root
-
-# wp plugin update --all --allow-root
-
-
- 
-# sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-# mkdir /run/php
-
-
-
-# wp redis enable --allow-root
-
+if ! [ -f /var/www/html/wp-config.php ]
+then
+	wp --allow-root --path=/var/www/html/ core download
+	wp --allow-root --path=/var/www/html/ config create --dbname=$db1_name --dbuser=$db1_user --dbpass=$db1_pwd --dbhost=mariadb:3306
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL --skip-email --allow-root
+	wp user create $db1_user $WP_UEMAIL --role=author --user_pass=$db1_pwd --allow-root
+	wp theme install astra --activate --allow-root
+else
+	echo the config file is already in there !!
+fi
 /usr/sbin/php-fpm8.2 -F
